d7828f352f40c3a28b84dfa185b7de8e04daafc1
hubangmao <1414430109@qq.com>
2016-07-19 11:07:08 +0800

    注册界面切换至登陆界面

--- src/cn/ucai/superwechat/activity/RegisterActivity.java
+++ src/cn/ucai/superwechat/activity/RegisterActivity.java
@@ -6,10 +6,6 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package cn.ucai.superwechat.activity;
 
@@ -120,7 +116,6 @@ public class RegisterActivity extends BaseActivity {
     public void back(View view) {
         finish();
     }
-
     //注册界面切换至登陆界面
     public void login(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
