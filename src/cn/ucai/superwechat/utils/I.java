933fe6cc3b9f01ecaa9c5d6eb2d5f00dece9519e
hubangmao <1414430109@qq.com>
2016-07-20 20:20:13 +0800

    I接口添加mes_变量

--- src/cn/ucai/superwechat/utils/I.java
+++ src/cn/ucai/superwechat/utils/I.java
@@ -5,7 +5,7 @@ import cn.ucai.superwechat.R;
 public interface I {
     String SERVER_URL = "http://10.0.2.2:8080/SuperWeChatServer/Server";
     String QUESTION = "?";
-
+    String MSG_PREFIX_MSG = "mes_";
     interface User {
         String TABLE_NAME = "t_superwechat_user";
         String USER_NAME = "m_user_name";                    //用户账号
