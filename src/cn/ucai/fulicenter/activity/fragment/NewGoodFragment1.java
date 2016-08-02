f6bfc0d1e8e8640bca26fec76e63a8df48ba3f36
hubangmao <1414430109@qq.com>
2016-08-02 11:22:51 +0800

    2.新品首页数据填充图片还未填充

--- src/cn/ucai/fulicenter/activity/fragment/NewGoodFragment1.java
+++ src/cn/ucai/fulicenter/activity/fragment/NewGoodFragment1.java
@@ -8,15 +8,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.activity.adapter.NewGoodsAdapter;
 import cn.ucai.fulicenter.bean.fulibean.NewGoodBean;
+import cn.ucai.fulicenter.data.OkHttpUtils2;
+import cn.ucai.fulicenter.utils.F;
+import cn.ucai.fulicenter.utils.Utils;
 
 /**
  * 新品Fragment
@@ -29,12 +35,70 @@ public class NewGoodFragment1 extends Fragment {
     GridLayoutManager mGrid;
     NewGoodsAdapter mGoodsAdapter;
     ArrayList<NewGoodBean> mList;
+    public static int PAGE_ID = 1;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mContext = getActivity();
         initView();
+        setListener();
+        initData();
+    }
+
+    private void setListener() {
+        //下拉刷新
+        mSwipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initData();
+                mSwipe.setRefreshing(false);
+            }
+        });
+        //上拉加载
+        mRecycler.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+    private void initData() {
+        String goodsRrl = F.SERVIEW_URL + "request=find_new_boutique_goods&cat_id=" +
+                F.CAT_ID + "&page_id=" + PAGE_ID +
+                "&page_size=" + F.PAGE_SIZE_DEFAULT;
+        Log.i("main", "bean" + goodsRrl);
+
+        final OkHttpUtils2<NewGoodBean[]> utils = new OkHttpUtils2<NewGoodBean[]>();
+        utils.url(goodsRrl)
+                .targetClass(NewGoodBean[].class)
+                .execute(new OkHttpUtils2.OnCompleteListener<NewGoodBean[]>() {
+                    @Override
+                    public void onSuccess(NewGoodBean[] result) {
+                        if (result == null) {
+                            Utils.toast(mContext, "网络错误");
+                            return;
+                        }
+                        Log.i("main", "result=" + result);
+                        ArrayList<NewGoodBean> bean = utils.array2List(result);
+                        mGoodsAdapter.upAdapterData(bean);
+                        mSwipe.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Utils.toast(mContext, "网络错误");
+                    }
+                });
+
+
     }
 
     private void initView() {
