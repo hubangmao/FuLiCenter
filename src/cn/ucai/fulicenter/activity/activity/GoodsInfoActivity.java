0f1757cec0f4d1aea17c0de1f24bf41d81fa7998
hubangmao <1414430109@qq.com>
2016-08-09 15:21:13 +0800

    4.分享平台相关jar包

--- src/cn/ucai/fulicenter/activity/activity/GoodsInfoActivity.java
+++ src/cn/ucai/fulicenter/activity/activity/GoodsInfoActivity.java
@@ -11,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.FuLiCenterApplication;
@@ -26,7 +28,6 @@ import cn.ucai.fulicenter.super_activity.BaseActivity;
 import cn.ucai.fulicenter.super_activity.LoginActivity;
 import cn.ucai.fulicenter.task.DowCollectTask;
 import cn.ucai.fulicenter.utils.F;
-import cn.ucai.fulicenter.utils.I;
 import cn.ucai.fulicenter.utils.Utils;
 
 public class GoodsInfoActivity extends BaseActivity implements View.OnClickListener {
@@ -163,7 +164,7 @@ public class GoodsInfoActivity extends BaseActivity implements View.OnClickListe
                 break;
             //分享
             case R.id.iv_goods_info_share:
-
+                showShare();
                 break;
         }
     }
@@ -179,6 +180,35 @@ public class GoodsInfoActivity extends BaseActivity implements View.OnClickListe
         } else {
             startActivity(new Intent(this, LoginActivity.class));
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     //添加收藏
